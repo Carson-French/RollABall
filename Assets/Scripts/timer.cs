0494c865394c2be1f702b13b25231878d2764248
Carson French <tfrench2@harding.edu>
2022-09-06 11:35:39 -0500

    Added Working Timer and Updated Version to 2020.3.38f1

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -1,20 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
-    public GameObject playerObject;
+    public float timeValue = 0;
+    public Text timerText;
+
 
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        playerObject.SendMessage("testMethod");
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
     }
 
     // Update is called once per frame
-    void Update()
+    void DisplayTime(float timeToDisplay)
     {
-        // New comment
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
